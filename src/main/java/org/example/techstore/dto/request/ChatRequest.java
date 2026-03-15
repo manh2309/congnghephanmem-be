bae12357d1791e46290f4796ab05520161911be1
Nguyen Tien Manh <manhnt.eledevo@gmail.com>
2026-03-15 20:07:49 +0700

    ManhNT: create API chat with AI

---
+++ src/main/java/org/example/techstore/dto/request/ChatRequest.java
@@ -0,0 +1,12 @@
+package org.example.techstore.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+    private String message;
+}
